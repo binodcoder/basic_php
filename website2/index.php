e7cf78ad4e93104a43dd2ac326ca779e4fb58d49
Binod Bhandari <binodcoder@gmail.com>
2023-07-22 09:39:41 +0545

    website2

---
+++ website2/index.php
@@ -0,0 +1,19 @@
+<?php include 'server_info.php'; ?>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" </title>
+</head>
+
+<body>
+    <div class="container">
+        <h1>Server & File Info</h1>
+        <?php 
+        if($server)
+         ?>
+    </div>
+</body>
+
+</html>
